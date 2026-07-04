c11ed39d686175509ee08a7e6f5a975e2e8bce65
dennis <machariad196@gmail.com>
2026-07-04 13:34:34 +0300

    add environment configuration files for production and development

---
+++ src/environments/environments.prod.ts
@@ -0,0 +1,6 @@
+export const environment = {
+    production: true,
+    apiUrl: 'https://nori.cybpact.co.ke/api',
+    appName: 'VoteGrip',
+    logLevel: 'error'
+};
---
+++ src/environments/environments.ts
@@ -0,0 +1,7 @@
+export const environment = {
+    production: false,
+    apiUrl: 'https://nori.cybpact.co.ke/api',
+    apiVersion: 'v1',
+    appName: 'VoteGrip',
+    logLevel: 'debug'
+};
