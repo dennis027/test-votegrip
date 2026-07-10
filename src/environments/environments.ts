692ba23f5cb19a35fb15cdcef491ba55effaaf6c
dennis <machariad196@gmail.com>
2026-07-10 17:00:54 +0300

    refactor: update OTP box styles and adjust submit button background color

--- src/environments/environments.ts
+++ src/environments/environments.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: false,
-    apiUrl: 'https://nori.cybpact.co.ke/api',
+    apiUrl: 'http://localhost:8000/api',
     apiVersion: 'v1',
     appName: 'VoteGrip',
     logLevel: 'debug'
