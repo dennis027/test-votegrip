842f1f41a0adac1dfb4ef5dce45b5f7a72cee865
dennis <machariad196@gmail.com>
2026-08-07 05:06:54 +0300

    getting county constituency and wards in my frontend app

--- src/environments/environments.ts
+++ src/environments/environments.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: false,
-    apiUrl: 'http://localhost:8000/api',
+    apiUrl: 'https://nori.cybpact.co.ke/api',
     apiVersion: 'v1',
     appName: 'VoteGrip',
     logLevel: 'debug'
