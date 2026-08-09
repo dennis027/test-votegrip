19ded433c4112e5a7517ccd7a9295e3e26b94564
dennis <machariad196@gmail.com>
2026-08-10 00:20:04 +0300

    adding api to assign poling station

--- src/app/services/candidates/agents-service.ts
+++ src/app/services/candidates/agents-service.ts
@@ -9,6 +9,7 @@ import { AuthService } from '../auth/auth';
 })
 export class AgentsService {
     private addAgentUrl              = `${environment.apiUrl}/${environment.apiVersion}/auth/users/agents/`;
+    private assignPollingStationUrl  = `${environment.apiUrl}/${environment.apiVersion}/campaign/assignments/`;
 
     constructor(
     private http: HttpClient,
@@ -18,6 +19,17 @@ export class AgentsService {
 
     addAgentApi(payload: any): Observable<any> {
     const url = `${this.addAgentUrl}`;
+    const headers = this.authService.getAccessToken()
+      ? new HttpHeaders({
+          'Content-Type': 'application/json',
+          'Authorization': `Bearer ${this.authService.getAccessToken()}`
+        })
+      : new HttpHeaders({ 'Content-Type': 'application/json' });
+    return this.http.post<any>(url, payload, { headers });
+  }
+
+  assignPollingStationToAgent(payload: any): Observable<any> {
+    const url = `${this.assignPollingStationUrl}`;
     const headers = this.authService.getAccessToken()
       ? new HttpHeaders({
           'Content-Type': 'application/json',
